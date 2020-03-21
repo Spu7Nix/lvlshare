8dd71bd8276be2399e573b3ffecf5c0ee23b846b
Spu7Nix <spu7nix@outlook.com>
2020-03-21 22:21:59 +0100

    exporting

--- src/levelstring.rs
+++ src/levelstring.rs
@@ -1,82 +1,43 @@
 // useful things for dealing with gd level data
 
 use base64;
-use libflate::gzip;
+use libflate::{gzip, zlib};
+use std::fs;
 use std::io::Read;
 
-fn xor(data: Vec<u8>, key: u8) -> Vec<u8> {
-    let mut new_data = Vec::new();
+use std::path::PathBuf;
 
-    for b in data {
-        //let new_byte = u64::from(b).pow(key);
-        new_data.push(b ^ key)
-    }
-    new_data
-}
-/*fn base_64_decrypt(encoded: Vec<u8>) -> Vec<u8> {
+fn base_64_decrypt(encoded: Vec<u8>) -> Vec<u8> {
     let mut new_data = encoded;
     while new_data.len() % 4 != 0 {
         new_data.push(b'=')
     }
     base64::decode(String::from_utf8(new_data).unwrap().as_str()).unwrap()
-}*/
+}
 
-use quick_xml::events::Event;
+use quick_xml::events::{BytesEnd, BytesStart, BytesText, Event};
 use quick_xml::Reader;
 use quick_xml::Writer;
-//use std::fs;
+
+use std::io::BufReader;
 use std::io::Cursor;
-//use std::path::PathBuf;
 
-pub fn get_level_string(save: String, level_name: &str) -> Vec<u8> {
-    //decrypting the savefile
-    let xor = xor(save.as_bytes().to_vec(), 11);
-    let replaced = String::from_utf8(xor)
-        .unwrap()
-        .replace("-", "+")
-        .replace("_", "/")
-        .replace("\0", "");
-    let b64 = base64::decode(replaced.as_str()).unwrap();
-    let mut decoder = gzip::Decoder::new(&b64[..]).unwrap();
-    let mut buf = Vec::new();
-    decoder.read_to_end(&mut buf).unwrap();
-
-    //println!("{}", String::from_utf8(buf[..1000].to_vec()).unwrap());
-
-    //getting level string
-
-    let mut reader = Reader::from_str(std::str::from_utf8(&buf).unwrap());
+pub fn get_level_string(save_decryptor: impl Read, level_name: &str) -> Vec<u8> {
+    let mut reader = Reader::from_reader(BufReader::new(save_decryptor));
     reader.trim_text(true);
-
-    //let buf = Vec::<u8>::new();
-
-    // The `Reader` does not implement `Iterator` because it outputs borrowed data (`Cow`s)
-    //let mut level_string = String::new();
-
     let mut buf = Vec::new();
-    /*
-    take away the first few things:
 
-    <?xml version="1.0"?>
-    <plist version="1.0" gjver="2.0">
-        <dict>
-            <k>LLM_01</k>
-            <d>
-                <k>_isArr</k>
-                <t />
-    */
     for _ in 0..7 {
-        println!("{:?}", reader.read_event(&mut buf).unwrap());
+        reader.read_event(&mut buf).unwrap();
     }
-    let mut current_outer_key = String::new();
+
+    //thing that gets the level
     loop {
         match reader.read_event(&mut buf) {
             Ok(Event::Start(e)) => {
                 let name = e.name();
-                if name == b"k" {
-                    current_outer_key = reader.read_text("k", &mut Vec::new()).unwrap();
-                } else if name == b"d" {
-                    println!("reading from level in position \"{}\"", current_outer_key);
+                if name == b"d" {
+                    //println!("reading from level in position \"{}\"", current_outer_key);
                     let mut d_value_layers = 0; //incase there are any other d values in there, make sure to include them and not close when they close
                     let mut k2_detected = false;
                     let mut level_found = false;
@@ -96,7 +57,6 @@ pub fn get_level_string(save: String, level_name: &str) -> Vec<u8> {
                                                 level_found = true;
                                                 k2_detected = false;
                                             } else {
-                                                println!("expected {}, found {}", level_name, text);
                                                 not_the_level = true;
                                             }
                                         }
@@ -110,7 +70,6 @@ pub fn get_level_string(save: String, level_name: &str) -> Vec<u8> {
 
                             Ok(Event::Start(e)) => {
                                 if e.name() == b"d" {
-                                    println!("start: {:?}", e);
                                     d_value_layers += 1
                                 }
                                 if !not_the_level {
@@ -119,13 +78,19 @@ pub fn get_level_string(save: String, level_name: &str) -> Vec<u8> {
                             }
                             Ok(Event::End(e)) => {
                                 if e.name() == b"d" {
-                                    println!("end:   {:?}", e);
                                     if d_value_layers == 0 {
                                         buf.clear();
                                         if not_the_level {
                                             break;
                                         } else {
-                                            return writer.into_inner().into_inner();
+                                            let mut encoder =
+                                                gzip::Encoder::new(Vec::new()).unwrap();
+                                            std::io::copy(
+                                                &mut &writer.into_inner().into_inner()[..],
+                                                &mut encoder,
+                                            )
+                                            .unwrap();
+                                            return encoder.finish().into_result().unwrap();
                                         }
                                     } else {
                                         d_value_layers -= 1
@@ -158,118 +123,79 @@ pub fn get_level_string(save: String, level_name: &str) -> Vec<u8> {
             Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
             _ => (), // There are several other `Event`s we do not consider here
         }
-
-        // if we don't keep a borrow elsewhere, we can clear the buffer to keep memory usage low
     }
-
-    //NO NEED TO DECRYPT LEVEL STRING
-    /*//decrypting level string
-    let ls_b64 = base_64_decrypt(
-        level_string
-            .replace("-", "+")
-            .replace("_", "/")
-            .replace("\0", "")
-            .as_bytes()
-            .to_vec(),
-    );
-
-    //println!("{}", String::from_utf8(ls_b64.clone()).unwrap());
-
-    let mut ls_decoder = gzip::Decoder::new(&ls_b64[..]).unwrap();
-    let mut ls_buf = Vec::new();
-    ls_decoder.read_to_end(&mut ls_buf).unwrap();
-
-    String::from_utf8(ls_buf).unwrap()*/
 }
 
-/*
-
-pub fn encrypt_level_string(ls: String, old_ls: String, path: PathBuf) {
-    let file_content = fs::read_to_string(path.clone()).unwrap();
-
-    //decrypting the savefile
-    let xor_encrypted = xor(file_content.as_bytes().to_vec(), 11);
-    let replaced = String::from_utf8(xor_encrypted)
-        .unwrap()
-        .replace("-", "+")
-        .replace("_", "/")
-        .replace("\0", "");
-    let b64 = base64::decode(replaced.as_str()).unwrap();
-    let mut decoder = gzip::Decoder::new(&b64[..]).unwrap();
-    let mut buf = Vec::new();
-    decoder.read_to_end(&mut buf).unwrap();
-
-    //encrypt the ls
-    //encrypting level string
-    /*def encrypt(dls):
-    fin = gzip.compress(dls)
-    fin = base64.b64encode(fin)
-    fin = fin.decode("utf-8").replace('+', '-').replace('/', '_')
-    fin = 'H4sIAAAAAAAAC' + fin[13:]
-    return(fin)*/
-
+pub fn export_level(level_file: PathBuf, save_file: PathBuf, save_decryptor: impl Read) {
     //setting level string
 
-    let mut reader = Reader::from_str(std::str::from_utf8(&buf).unwrap());
+    let mut reader = Reader::from_reader(BufReader::new(save_decryptor));
     reader.trim_text(true);
 
     let mut writer = Writer::new(Cursor::new(Vec::new()));
 
+    //decompress the level file
+
+    let level_compressed = fs::File::open(level_file).expect("Cannot find specified file!");
+    let mut level_decompressor = gzip::Decoder::new(level_compressed).unwrap();
+
+    let mut level = Vec::new();
+    level_decompressor.read_to_end(&mut level).unwrap();
+
+    //write it in
     let mut buf = Vec::new();
+    for _ in 0..11 {
+        match reader.read_event(&mut buf) {
+            Ok(e) => writer.write_event(e).unwrap(),
+            Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
+        };
+    }
 
-    let mut k4_detected = false;
-    let mut done = false;
-    let mut k2_detected = false;
+    writer.write(b"<k>k_0</k><d>").unwrap();
 
-    //println!("{}", old_ls);
+    writer.write(&level).unwrap();
 
-    let full_ls = old_ls + &ls;
-
+    writer.write(b"</d>").unwrap();
+    let mut d_layer = 0;
+    let mut level_num = 1;
     loop {
         match reader.read_event(&mut buf) {
-            // unescape and decode the text event using the reader encoding
-            Ok(Event::Text(e)) => {
-                let text = e.unescape_and_decode(&reader).unwrap();
-                if k4_detected {
-                    let encrypted_ls: String = {
-                        let mut ls_encoder = gzip::Encoder::new(Vec::new()).unwrap();
-                        ls_encoder.write_all(&full_ls.as_bytes()).unwrap();
-                        let b64_encrypted =
-                            base64::encode(&ls_encoder.finish().into_result().unwrap());
-                        let fin = b64_encrypted.replace("+", "-").replace("/", "_");
-                        "H4sIAAAAAAAAC".to_string() + &fin[13..]
-                    };
+            Ok(Event::Start(e)) => {
+                let name = e.name();
+                if name == b"d" {
+                    d_layer += 1;
+                    writer.write_event(Event::Start(e)).unwrap();
+                } else if name == b"k" && d_layer == 0 {
+                    writer.write(b"<k>k_").unwrap();
+                    writer.write(level_num.to_string().as_bytes()).unwrap();
+                    writer.write(b"</k>").unwrap();
 
-                    assert!(writer
-                        .write_event(Event::Text(BytesText::from_plain_str(&encrypted_ls)))
-                        .is_ok());
-                    done = true;
-                    k4_detected = false;
+                    reader.read_event(&mut buf).unwrap();
+                    reader.read_event(&mut buf).unwrap();
+                    level_num += 1;
                 } else {
-                    assert!(writer.write_event(Event::Text(e)).is_ok())
-                }
-
-                if k2_detected {
-                    println!("Writing to level: {}", text);
-                    k2_detected = false;
-                }
-
-                if !done && text == "k4" {
-                    k4_detected = true
-                }
-
-                if !done && text == "k2" {
-                    k2_detected = true
+                    writer.write_event(Event::Start(e)).unwrap();
                 }
             }
-            Ok(Event::Eof) => break, // exits the loop when reaching end of file
-            Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
-            Ok(e) => assert!(writer.write_event(e).is_ok()),
-        }
 
-        // if we don't keep a borrow elsewhere, we can clear the buffer to keep memory usage low
-        buf.clear();
+            Ok(Event::End(e)) => {
+                if e.name() == b"d" {
+                    d_layer -= 1;
+                }
+                writer.write_event(Event::End(e)).unwrap();
+            }
+            Ok(Event::Eof) => break,
+
+            Ok(e) => {
+                writer.write_event(e).unwrap();
+            }
+
+            Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
+        };
+
+        buf.clear()
     }
+
     let bytes = writer.into_inner().into_inner();
     //encrypt level save
     use std::io::Write;
@@ -290,12 +216,16 @@ pub fn encrypt_level_string(ls: String, old_ls: String, path: PathBuf) {
     with_signature.extend(checksum.to_le_bytes().to_vec());
     with_signature.extend(data_size.to_le_bytes().to_vec());
 
-    let encoded = base64::encode(&with_signature)
-        .replace("+", "-")
-        .replace("/", "_")
-        .as_bytes()
-        .to_vec();
+    let mut encoded = Vec::new();
+    {
+        let mut encoder = base64::write::EncoderWriter::new(&mut encoded, base64::URL_SAFE);
+        encoder.write_all(&with_signature).unwrap();
+        encoder.finish().unwrap();
+    }
 
-    let fin = xor(encoded, 11);
-    assert!(fs::write(path, fin).is_ok());
-}*/
+    for b in encoded.iter_mut() {
+        *b ^= 11
+    }
+
+    fs::write(save_file, encoded).unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,45 @@ use std::env;
 use std::fs;
 
 use std::path::PathBuf;
+use std::time::Instant;
+
+use base64;
+use libflate::gzip;
 
 fn main() {
+    let start_time = Instant::now();
+
     let mut args = env::args();
     args.next();
 
     let gd_path = PathBuf::from(std::env::var("localappdata").expect("No local app data"))
         .join("GeometryDash/CCLocalLevels.dat");
 
+    let save_file = fs::File::open(gd_path.clone()).expect("Cannot find savefile!");
+    let mut xor = xorstream::Transformer::new(vec![11], save_file);
+    let b64 = base64::read::DecoderReader::new(&mut xor, base64::URL_SAFE);
+    let save_decryptor = gzip::Decoder::new(b64).unwrap();
+
     match args.next().expect("Expected command!").as_ref() {
         "export" => {
-            let file_content =
-                fs::read_to_string(gd_path).expect("Your local geometry dash files were not found");
-
             let level_name = args.next().unwrap();
 
-            let level = levelstring::get_level_string(file_content, &level_name);
+            let level = levelstring::get_level_string(save_decryptor, &level_name);
 
             use std::io::Write;
             let mut file = fs::File::create(format!("{}.lvl", level_name)).unwrap();
             file.write_all(&level).unwrap();
         }
 
+        "import" => {
+            levelstring::export_level(PathBuf::from(args.next().unwrap()), gd_path, save_decryptor);
+        }
+
         _ => panic!("Unknown command"),
     }
+
+    println!(
+        "Completed in {} milliseconds!",
+        start_time.elapsed().as_millis()
+    );
 }
